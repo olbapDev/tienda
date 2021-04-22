0db370ab75f1c968b457950cfb19b0cd331c2fc4
olbapDev <pa.garridoc@alumnos.duoc.cl>
2021-04-22 00:46:59 -0400

    hola mundo

---
+++ urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import UserRegisterView
+
+urlpatterns = [
+    #path('', views.home, name="home"),
+    path('register/', UserRegisterView.as_view(), name='register'), 
+]
